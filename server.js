e370f259947c1294e175d629d6d303e6da0560cc
StevenSymons <symons_steven@hotmail.com>
2019-06-09 17:04:37 +0200

    added bodyparser and testing route

--- server.js
+++ server.js
@@ -3,4 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+
+app.get("/api", (req, res) => {
+  res.send("This will be the home of our API. :)");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
